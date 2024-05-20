b6d39d77dc21e1117d12cf8cc60e9698bb9d2dc3
Jhon Arias <157815560+Arias-Hen@users.noreply.github.com>
2024-05-20 00:36:49 -0400

    Update ejercicio4.c

--- EXAMEN1ERPARCIAL317/ejercicio4/ejercicio4.c
+++ EXAMEN1ERPARCIAL317/ejercicio4/ejercicio4.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 
-#define N 4
-
 int main() {
-	int matriz[N][N];
+    int tamano;
+    printf("Introduce el tamaño de la matriz y el vector: ");
+    scanf("%d", &tamano);
 
-	int vector[N] = { 1, 2, 3, 4 }; 
-	int resultado[N] = { 0 };
+    int nueva_matriz[tamano][tamano];
+    int nuevo_vector[tamano];
+    int nuevo_resultado[tamano];
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			matriz[i][j] = i * N + j + 1;
-		}
-	}
+    printf("Matriz:\n");
+    for (int i = 0; i < tamano; i++) {
+        nuevo_vector[i] = rand() % 10 + 1; 
+        for (int j = 0; j < tamano; j++) {
+            nueva_matriz[i][j] = rand() % 10 + 1; 
+            printf("%d ", nueva_matriz[i][j]); 
+        }
+        printf("\n");
+    }
+
+    printf("\nVector:\n");
+    for (int i = 0; i < tamano; i++) {
+        printf("%d\n", nuevo_vector[i]);
+    }
+    
+    for (int i = 0; i < tamano; i++) {
+        nuevo_resultado[i] = 0;
+    }
 
 #pragma omp parallel for
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			resultado[i] += matriz[i][j] * vector[j];
-		}
-	}
+    for (int i = 0; i < tamano; i++) {
+        for (int j = 0; j < tamano; j++) {
+            nuevo_resultado[i] += nueva_matriz[i][j] * nuevo_vector[j];
+        }
+    }
 
+    printf("\nResultado:\n");
+    for (int i = 0; i < tamano; i++) {
+        printf("%d\n", nuevo_resultado[i]);
+    }
 
-	printf("Resultado:\n");
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", resultado[i]);
-	}
-
-	return 0;
+    return 0;
 }
