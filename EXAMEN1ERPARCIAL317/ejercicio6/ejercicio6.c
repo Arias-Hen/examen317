31cf0182e5e402ea85792110d59a4329a3779eed
Jhon Arias <157815560+Arias-Hen@users.noreply.github.com>
2024-05-20 00:38:05 -0400

    Update ejercicio6.c

--- EXAMEN1ERPARCIAL317/ejercicio6/ejercicio6.c
+++ EXAMEN1ERPARCIAL317/ejercicio6/ejercicio6.c
@@ -3,36 +3,43 @@
 #include <math.h>
 #include <mpi.h>
 
-#define NUM_PUNTOS 1000000
+#define NUM_INTERVALOS 1000000
+
+double f(double x) {
+    return 4.0 / (1.0 + x * x);
+}
 
 int main(int argc, char *argv[]) {
-	int rank, size;
-	double x, y;
-	int contador = 0;
-	double pi;
-	int total_contador;
+    int rank, size;
+    double suma_parcial = 0.0;
+    double integral_parcial = 0.0;
+    double integral_total;
+    double inicio_intervalo, fin_intervalo;
+    double ancho_intervalo;
+    int i;
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	srand(rank);
+    ancho_intervalo = 1.0 / (double)NUM_INTERVALOS;
 
-	for (int i = 0; i < NUM_PUNTOS / size; i++) {
-		x = (double)rand() / RAND_MAX;
-		y = (double)rand() / RAND_MAX;
-		if (x * x + y * y <= 1.0) {
-			contador++;
-		}
-	}
+    inicio_intervalo = rank * (double)NUM_INTERVALOS / size * ancho_intervalo;
+    fin_intervalo = (rank + 1) * (double)NUM_INTERVALOS / size * ancho_intervalo;
 
-	MPI_Reduce(&contador, &total_contador, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    for (i = 0; i < NUM_INTERVALOS / size; i++) {
+        double x = inicio_intervalo + i * ancho_intervalo;
+        suma_parcial += f(x);
+    }
 
-	if (rank == 0) {
-		pi = 4.0 * total_contador / NUM_PUNTOS;
-		printf("Valor de pi: %lf\n", pi);
-	}
+    integral_parcial = ancho_intervalo * suma_parcial;
 
-	MPI_Finalize();
-	return 0;
+    MPI_Reduce(&integral_parcial, &integral_total, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("Valor de pi: %lf\n", integral_total);
+    }
+
+    MPI_Finalize();
+    return 0;
 }
