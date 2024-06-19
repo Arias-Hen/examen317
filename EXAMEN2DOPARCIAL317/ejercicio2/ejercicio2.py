126990d633ca1eafa382ec7acc45510bdbf7c948
Jhon Arias <157815560+Arias-Hen@users.noreply.github.com>
2024-06-18 23:42:11 -0400

    ejercicio2.py

--- EXAMEN2DOPARCIAL317/ejercicio2/ejercicio2.py
+++ EXAMEN2DOPARCIAL317/ejercicio2/ejercicio2.py
@@ -1,3 +1,4 @@
+# EJERCICIO 2
 import cv2
 import scipy.sparse as sp
 
@@ -8,4 +9,6 @@ img1 = cv2.imread("IM1.jpg");
 img2 = cv2.imread("IM2.jpg");
 m_sparse1 = sp.coo_matrix(img1[:,:,1])
 m_sparse2 = sp.coo_matrix(img2[:,:,1])
-print(m_sparse2)
+matriz_Sparce = m_sparse1 + m_sparse2
+
+print(matriz_Sparce)
