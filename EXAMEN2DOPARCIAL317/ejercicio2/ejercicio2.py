6428b1a4cac280f8ec028addc811d3d936997e80
Jhon Arias <157815560+Arias-Hen@users.noreply.github.com>
2024-06-18 23:08:04 -0400

    ejercicio2.py

--- EXAMEN2DOPARCIAL317/ejercicio2/ejercicio2.py
+++ EXAMEN2DOPARCIAL317/ejercicio2/ejercicio2.py
@@ -1,4 +1,9 @@
+import cv2
 import scipy.sparse as sp
+
+drive.mount("/content/drive")
+os.chdir("/content/drive/MyDrive/INF317")
+
 img1 = cv2.imread("IM1.jpg");
 img2 = cv2.imread("IM2.jpg");
 m_sparse1 = sp.coo_matrix(img1[:,:,1])
